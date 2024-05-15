f228726fce3e135317806d4ea10f11efe2ca9421
Perumalla Venkata Naga Dhansush <perumalladhanush102@gmail.com>
2024-05-15 21:51:31 +0530

    Ready for the Deployment

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask,send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-
+import os
 app = Flask(__name__)
 
 CORS(app)
@@ -14,6 +14,16 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # Import routes after db initialization to avoid circular imports
+frontend_folder = os.path.join(os.getcwd(),"..","frontend")
+dist_folder = os.path.join(frontend_folder,"dist")
+
+@app.route("/",defaults={"filename":""})
+@app.route("/<path:filename>")
+def index(filename):
+  if not filename:
+    filename = "index.html"
+  return send_from_directory(dist_folder,filename)
+
 import routes
 
 with app.app_context():
